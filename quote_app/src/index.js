241c9bffdcdec9723bb8db627d319ad3446e8941
Priyadarshi Roy <37247296+PriyadarshiRoy@users.noreply.github.com>
2018-03-25 10:30:14 +0530

    Update index.js

--- quote_app/src/index.js
+++ quote_app/src/index.js
@@ -1,7 +1,7 @@
 import React from  'react';
 import ReactDOM from 'react-dom';
 import Quote from './Quote';
-import 'tachyons'; 
+import './tachyons.css'; 
 import './index.css';
 
 class App extends React.Component{
